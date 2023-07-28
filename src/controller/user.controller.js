daf90bead34e703c417a95fc74f40f2c857aaa8d
yhza <2833553280@qq.com>
2023-07-28 12:18:25 +0800

    6-将路由和控制器拆分

---
+++ src/controller/user.controller.js
@@ -0,0 +1,10 @@
+class Usercontroller {
+     async register(ctx,next){
+        ctx.body ='注册成功'
+     }
+     async login(ctx,next){
+        ctx.body ='登录成功'
+     }
+}
+
+module.exports= new Usercontroller()
--- src/main.js
+++ src/main.js
@@ -1,12 +1,6 @@
-const Koa = require('koa');
+const  app = require('./app/index')
+
 const {APP_POST} = require('./config/config.default');
-
-const app = new Koa();
-
-// 中间件
-app.use((ctx,next)=>{
-    ctx.body ='hello 哈哈哈'
-})
 
 app.listen(APP_POST,()=>{
     console.log(`server listening on http://localhost:${APP_POST}`);
