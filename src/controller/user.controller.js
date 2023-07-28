615a1da2ae1d4cbd1430f6c3ba6cb4dabfc600b0
yhza <2833553280@qq.com>
2023-07-28 12:54:39 +0800

    06-body解析

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,6 +1,15 @@
+const {createUser} = require('../service/user.service');
+
 class Usercontroller {
      async register(ctx,next){
-        ctx.body ='注册成功'
+        // 1.获取数据
+        // console.log(ctx.request.body);
+        const {user_name, password}= ctx.request.body;
+        // 2.操作数据库
+        const res= await createUser(user_name, password);
+        console.log(res);
+        // 3.返回结果
+        ctx.body = ctx.request.body;
      }
      async login(ctx,next){
         ctx.body ='登录成功'
